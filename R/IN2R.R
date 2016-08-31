ad64f8f408ec2a5ab4f5b08a8542e7280f140b10
FluffierThanThou <fluffy.l2032@gmail.com>
2016-08-31 16:33:56 +0200

    fix columns in parameters

--- R/IN2R.R
+++ R/IN2R.R
@@ -2,6 +2,7 @@
 #' 
 #' @param f NULL, file location, or chooser if left NULL.
 #' @return MCAT_itembank
+#' @import stringr
 #' @export
 IN2R <- function(f = NULL) {
   if (is.null(f)) f <- file.choose()
@@ -10,6 +11,7 @@ IN2R <- function(f = NULL) {
   lines <- readLines(file(f))
   x <- strsplit(str_trim(lines),"\\s+")
   
+  # first 5 elements on second line
   N <- as.integer(x[[2]][1])
   M <- as.integer(x[[2]][2])
   Q <- as.integer(x[[2]][3])
@@ -19,6 +21,7 @@ IN2R <- function(f = NULL) {
   covar <- matrix(NA,Q,Q)
   mean <- numeric(Q)
   
+  # lines 3-(3+Q) are variance covariance matrix and mean vector
   for (i in 3:(Q+2)){
     q <- i-2
     line <- as.numeric(x[[i]])
@@ -33,7 +36,8 @@ IN2R <- function(f = NULL) {
   start <- 3 + 2*Q # offset for mean/covariance + starting items.
   end <- length(x)
   
-  k <- 0
+  # keep track of item number
+  k <- -1
   
   for (i in start:end){
     line <- as.numeric(x[[i]])
@@ -41,13 +45,12 @@ IN2R <- function(f = NULL) {
     if (line[1] > k){ # this is a new item
       k <- line[1]
       m[k] <- line[2]
-      a[k,] <- line[4:(3+Q)]
+      a[k,] <- line[3:(2+Q)]
     } else { # this is a category
-      b[k,line[2]] <- line[4]
+      b[k,line[2]] <- line[3]
     }
   }
   
-  out <- list(alpha=a,beta=b,guessing=matrix(0,K,1),mean=mean,covar=covar,m=m,N=N,M=M,Q=Q,K=K,model='GPCM')
-  class(out) <- 'MCAT_itembank'
+  out <- list(alpha=a,beta=b,guessing=matrix(0,K,1),mean=mean,covar=covar,m=m,N=N,M=M,Q=Q,K=K)
   return(invisible(out))
 }
